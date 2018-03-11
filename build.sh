920997c3671c83ee35156208fd188b34536b4941
etienne <squawkcpp@gmail.com>
2018-03-11 12:58:56 +0100

    conan buildfile

--- build.sh
+++ build.sh
@@ -2,29 +2,12 @@
 
 set -e
 
-IMAGE_NAME=spielhuus/toolchain
+IMAGE=spielhuus/toolchain:1.0.1
 TAG=`if [ -z "$2" ]; then echo "master"; else echo "$2" ; fi`
-PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/build $IMAGE_NAME /bin/sh)
-echo "build lightningcpp tag:$TAG, image:$PID"
+PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/sh)
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
-
-$DOCKER_EXEC "cd build && conan install /repo --build=missing"
-$DOCKER_EXEC "cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -Dbuild_tests=on -Dbuild_example_server=on -Dbuild_documentation=on -DLIGHTNING_TAG_VERSION=$TAG"
-$DOCKER_EXEC "cmake --build /build"
-$DOCKER_EXEC "cmake --build /build --target test"
-$DOCKER_EXEC "cmake --build /build --target doc"
-$DOCKER_EXEC "cmake --build /build --target package"
-
-if [ -z "$1" ]; then
-	echo not deploying to bintray, set api key to do so.
-else
-        $DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest"
-	$DOCKER_EXEC "conan install lightningcpp/$TAG@conan-cpp/latest --build=lightningcpp"
-	$DOCKER_EXEC "conan user -p $1 -r conan-cpp squawkcpp"
-	$DOCKER_EXEC "conan upload lightningcpp/$TAG@conan-cpp/latest --all -r=conan-cpp"
-fi
-
-sudo docker build -f docker/Dockerfile --build-arg LIGHTNING_TAG_VERSION=$TAG -t lightning .
+$DOCKER_EXEC "conan user -p $1 -r conan-cpp squawkcpp"
+$DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install lightningcpp/0.1.0@conan-cpp/latest --build=missing && conan upload lightningcpp/0.1.0@conan-cpp/latest --all -r=conan-cpp"
 
 sudo docker rm -f $PID
